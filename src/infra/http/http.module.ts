1cb0539a00dfae86d40e03d8d6fa5111bb14f339
André Viapiana <andreviapiana@gmail.com>
2023-11-30 16:12:47 -0300

    ✨feat: Desacoplando camadas no NestJS - Implementando Repositórios do Prisma

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,7 +4,7 @@ import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { PrismaService } from '../prisma/prisma.service'
+import { PrismaService } from '../database/prisma/prisma.service'
 
 @Module({
   controllers: [
