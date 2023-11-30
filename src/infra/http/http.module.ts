9bf12089fd84eca74dddcc96a16b97db7d5c1c2c
André Viapiana <andreviapiana@gmail.com>
2023-11-30 18:11:32 -0300

    ✨feat: Desacoplando camadas no NestJS - Comunicação entre Camadas

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,18 +1,19 @@
 import { Module } from '@nestjs/common'
-
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { PrismaService } from '../database/prisma/prisma.service'
+import { DatabaseModule } from '../database/database.module'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [PrismaService],
+  providers: [CreateQuestionUseCase],
 })
 export class HttpModule {}
