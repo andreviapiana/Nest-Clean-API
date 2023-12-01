d8139dd2623f7311ea1f388d9841e7e770ba2ffd
André Viapiana <andreviapiana@gmail.com>
2023-12-01 16:07:51 -0300

    ✨feat: Desacoplando camadas no NestJS - Rotas Criando EnvModule

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
-import { Env } from '@/infra/env'
 import { JwtStrategy } from './jwt.strategy'
 import { JwtAuthGuard } from './jwt-auth.guard'
 import { APP_GUARD } from '@nestjs/core'
+import { EnvService } from '../env/env.service'
+import { EnvModule } from '../env/env.module'
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
-      inject: [ConfigService],
+      imports: [EnvModule],
+      inject: [EnvService],
       global: true,
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
-        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
-
+      useFactory(env: EnvService) {
+        const privateKey = env.get('JWT_PRIVATE_KEY')
+        const publicKey = env.get('JWT_PUBLIC_KEY')
         return {
           signOptions: { algorithm: 'RS256' },
           privateKey: Buffer.from(privateKey, 'base64'),
@@ -27,6 +27,7 @@ import { APP_GUARD } from '@nestjs/core'
   ],
   providers: [
     JwtStrategy,
+    EnvService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
