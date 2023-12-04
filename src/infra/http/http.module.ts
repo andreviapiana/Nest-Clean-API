ef04fed57f64af49ba2906cd8349706db72dcac8
André Viapiana <andreviapiana@gmail.com>
2023-12-04 18:46:21 -0300

    ✨feat: Implementando Controllers e Testes - Controller: Editar Resposta

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -17,6 +17,8 @@ import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/dele
 import { DeleteQuestionController } from './controllers/delete-question.controller'
 import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
 import { AnswerQuestionController } from './controllers/answer-question.controller'
+import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer'
+import { EditAnswerController } from './controllers/edit-answer.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -29,6 +31,7 @@ import { AnswerQuestionController } from './controllers/answer-question.controll
     EditQuestionController,
     DeleteQuestionController,
     AnswerQuestionController,
+    EditAnswerController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -39,6 +42,7 @@ import { AnswerQuestionController } from './controllers/answer-question.controll
     EditQuestionUseCase,
     DeleteQuestionUseCase,
     AnswerQuestionUseCase,
+    EditAnswerUseCase,
   ],
 })
 export class HttpModule {}
