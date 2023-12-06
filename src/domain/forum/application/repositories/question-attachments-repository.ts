f23fa16e93dee835c44860f04e8a3c42d8c5b8f2
André Viapiana <andreviapiana@gmail.com>
2023-12-06 10:27:14 -0300

    ✨feat: Uploads e Relacionamentos – Criando Pergunta com Anexos

--- src/domain/forum/application/repositories/question-attachments-repository.ts
+++ src/domain/forum/application/repositories/question-attachments-repository.ts
@@ -1,8 +1,8 @@
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
 export abstract class QuestionAttachmentsRepository {
-abstract createMany(attachments: QuestionAttachment[]): Promise<void>
-abstract deleteMany(attachments: QuestionAttachment[]): Promise<void>
+  abstract createMany(attachments: QuestionAttachment[]): Promise<void>
+  abstract deleteMany(attachments: QuestionAttachment[]): Promise<void>
 
   abstract findManyByQuestionId(
     questionId: string,
